5e3954ee849c2a05fbad997849be50ecddc8c966
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:51 -0500

    TUnit conversion

--- test/Soenneker.Windmill.OpenApiClientUtil.Tests/WindmillOpenApiClientUtilTests.cs
+++ test/Soenneker.Windmill.OpenApiClientUtil.Tests/WindmillOpenApiClientUtilTests.cs
@@ -1,20 +1,19 @@
 using Soenneker.Windmill.OpenApiClientUtil.Abstract;
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Windmill.OpenApiClientUtil.Tests;
 
-[Collection("Collection")]
-public sealed class WindmillOpenApiClientUtilTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class WindmillOpenApiClientUtilTests : HostedUnitTest
 {
     private readonly IWindmillOpenApiClientUtil _openapiclientutil;
 
-    public WindmillOpenApiClientUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public WindmillOpenApiClientUtilTests(Host host) : base(host)
     {
         _openapiclientutil = Resolve<IWindmillOpenApiClientUtil>(true);
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
