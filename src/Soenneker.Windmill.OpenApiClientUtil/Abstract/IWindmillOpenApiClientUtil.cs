7c56bb00253d822354c8464f695dbf6fbc76e0bb
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:39:00 -0500

    xml commenting

--- src/Soenneker.Windmill.OpenApiClientUtil/Abstract/IWindmillOpenApiClientUtil.cs
+++ src/Soenneker.Windmill.OpenApiClientUtil/Abstract/IWindmillOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Windmill.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IWindmillOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<WindmillOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
